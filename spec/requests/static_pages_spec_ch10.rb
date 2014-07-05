397770c28ef0945669fc230a464aaafbdff766f8
ZebGirouard <timzebgir@gmail.com>
2014-07-05 15:43:54 -0400

    Finish ch 11

--- spec/requests/static_pages_spec_ch10.rb
+++ spec/requests/static_pages_spec_ch10.rb
@@ -18,29 +18,37 @@ describe "Static pages" do
     it { should_not have_title('| Home') }
 
     describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem")
-        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
-        sign_in user
-        visit root_path
-      end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          expect(page).to have_selector("li##{item.id}", text: item.content)
+      describe "only one post" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+          sign_in user
+          visit root_path
+        end
+        it "should not have an s if only one micropost" do
+          expect(page).to have_no_content('microposts')
         end
       end
 
-      describe "follower/following counts" do
-        let(:other_user) { FactoryGirl.create(:user) }
+      describe "forty posts" do
+        let(:user) { FactoryGirl.create(:user) }
         before do
-          other_user.follow!(user)
+          40.times {FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")}
+          sign_in user
           visit root_path
         end
 
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
+        describe "pagination" do
+          it { should have_selector('div.pagination') }
+        end
+        
+        it "should render the user's feed" do
+          expect(page).to have_content('microposts')
+          user.feed.first(30).each do |item|
+            expect(page).to have_selector("li##{item.id}", text: item.content)
+          end
+        end
       end
     end
   end
